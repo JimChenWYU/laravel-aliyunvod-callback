e1e7e89db3cebcf48d9cebe247c18e3ab10a3e99
JimChen <imjimchen@163.com>
2019-08-25 10:35:02 +0800

    ucfirst __call get parameter
    Signed-off-by: JimChen <imjimchen@163.com>

--- src/Message.php
+++ src/Message.php
@@ -26,7 +26,7 @@ class Message extends Collection
         if (Str::startsWith($method, 'get')) {
             $messageParameter = Str::replaceFirst('get', '', $method);
 
-            return $this->get($messageParameter);
+            return $this->get(ucfirst($messageParameter));
         }
 
         return parent::__call($method, $parameters);
